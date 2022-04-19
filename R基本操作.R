221b11e2df0c76d2a541859aa9a265661ee7cc34
takuma123-type <72244249+takuma123-type@users.noreply.github.com>
2022-04-19 11:50:47 +0900

    基本操作

---
+++ R基本操作.R
@@ -0,0 +1,23 @@
+# # Rの基本操作
+# sin(pi/6)+cos(pi/3)+sqrt(9)+log(exp(1))
+
+# # 代入演算子
+# a <- 123
+# a
+
+# #変数命名
+# data.train <- Hello World
+
+# data.test <- ご飯食べ
+
+#変数に確認
+
+#制御構文
+
+a<-"犬"
+switch(a,
+        "犬"=print("わん"),
+        "猫"=print("にゃん"),
+        "鳥"=print("ぴよ"),
+        print("ぎゃふん")
+)
