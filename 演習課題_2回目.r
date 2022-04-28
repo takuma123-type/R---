afeeb11038b29aa5413d1be3bb74c92f33e0e127
Takuma <72244249+takuma123-type@users.noreply.github.com>
2022-04-28 11:59:25 +0900

    参考サイトを追加

--- 演習課題_2回目.r
+++ 演習課題_2回目.r
@@ -10,3 +10,14 @@ aggregate(score~department,data=data,FUN=mean)
 #学科×クラス別の平均値
 aggregate(score~department*class,data=data,FUN=mean)
 
+
+
+参考にしたサイト：
+#データフレーム ⇔ リスト の変換
+URL:https://qiita.com/U25CE/items/bf30ea2fcc79ba399dd0
+
+#Rコード:データフレームのグループごと（セルごと）の平均を出す。
+URL:https://toukei.link/programmingandsoftware/r/aggregatefunction/
+
+#ベクトルの作成
+URL:http://cse.naro.affrc.go.jp/takezawa/r-tips/r/12.html
